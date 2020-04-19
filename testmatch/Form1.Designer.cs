ed2d65a26ac1c71066f78fe008132b5fa5fbdd72
panupongkongkate <61161511+panupongkongkate@users.noreply.github.com>
2020-04-19 12:28:04 +0700

    Template matching-multiple objects using Emgu (C#)

--- testmatch/Form1.Designer.cs
+++ testmatch/Form1.Designer.cs
@@ -29,12 +29,113 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(59, 20);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(408, 549);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Location = new System.Drawing.Point(492, 62);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(463, 500);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox3.Location = new System.Drawing.Point(989, 30);
+            this.imageBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(532, 611);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox3.TabIndex = 4;
+            this.imageBox3.TabStop = false;
+            this.imageBox3.Click += new System.EventHandler(this.imageBox3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 596);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 86);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ภาพ1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 594);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 90);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "ภาพ2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(503, 596);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 86);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "ประมวลผล";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1533, 706);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- testmatch/Form1.cs
+++ testmatch/Form1.cs
@@ -7,14 +7,140 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Diagnostics;
 
 namespace testmatch
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog op1 = new OpenFileDialog();
+        Image<Bgr, byte> source = null;
+        Image<Bgr, byte> template = null;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (op1.ShowDialog() == DialogResult.OK)
+            {
+                source = new Image<Bgr, byte>(op1.FileName);
+                imageBox1.Image = source;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (op1.ShowDialog() == DialogResult.OK)
+            {
+                template = new Image<Bgr, byte>(op1.FileName);
+                imageBox2.Image = template;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if ((source != null) && (template != null))
+            {
+
+                List<Rectangle> rects = new List<Rectangle>();
+
+                Image<Bgr, byte> imageToShow = source.Copy();
+
+             
+
+
+                using (Image<Gray, float> result = source.MatchTemplate(template, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+                {
+                    //แปลงภาพเทา
+                    // Image<Gray, byte> result = source.Convert<Gray, byte>();
+
+                    //ทำการ MatchTemplate 
+
+                    //CvInvoke.MatchTemplate(source, template, result, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+
+
+                    //กำหนดค่า min max ของภาพ
+                    double[] minValues, maxValues;
+
+                    Point[] minLocations, maxLocations;
+
+                    //หาค่าmin max ของภาพ 1 
+                    result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+
+
+
+
+                    //ทำการวาดภาพ
+
+                    //// You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+
+                    float[,,] match00 = result.Data;
+                    for (int x = 0; x < match00.GetLength(1); x++)
+                    {
+                        for (int y = 0; y < match00.GetLength(0); y++)
+                        {
+                            double matchsouce = match00[y, x, 0];
+                            if (matchsouce > 0.9)
+                            {
+                                Rectangle bect = new Rectangle(new Point(x, y), template.Size);
+                                imageToShow.Draw(bect, new Bgr(Color.Red), 5);
+
+                                rects.Add(bect);
+
+                            }
+
+                        }
+
+
+                        /*
+                         if (maxValues[0] > threshold)
+                         {
+
+                             Rectangle match = new Rectangle(maxLocations[0] , template.Size);
+                             CvInvoke.Rectangle(imageToShow, match, new MCvScalar(0,0,255), 5);
+                             imageBox3.Image = imageToShow;
+
+                         }
+                         */
+
+
+                    }
+                    imageBox3.Image = imageToShow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+            }
+        }
+
+        private void imageBox3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
